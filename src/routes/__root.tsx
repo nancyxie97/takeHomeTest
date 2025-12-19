0affb461d92a354e437219216fe67104df7dbd70
Nancy Xie <33169285+nancyxie97@users.noreply.github.com>
2025-12-19 12:29:35 -0500

    style
    - made padding all round the same

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,26 +1,26 @@
 import NavBar from "@/components/NavBar";
 import { useDarkMode } from "@/stores/DarkModeStore";
 import {
-  createRootRoute,
-  Outlet,
-  useRouterState,
+	createRootRoute,
+	Outlet,
+	useRouterState,
 } from "@tanstack/react-router";
 
 export const Route = createRootRoute({
-  component: RouteComponent,
+	component: RouteComponent,
 });
 
 function RouteComponent() {
-  useDarkMode();
-  const pathname = useRouterState({
-    select: (s) => s.location.pathname,
-  });
-  return (
-    <div className="bg-background text-foreground">
-      <NavBar />
-      <div key={pathname} className="animate-fade-in h-full p-6 pt-2">
-        <Outlet />
-      </div>
-    </div>
-  );
+	useDarkMode();
+	const pathname = useRouterState({
+		select: (s) => s.location.pathname,
+	});
+	return (
+		<div className="bg-background text-foreground">
+			<NavBar />
+			<div key={pathname} className="animate-fade-in h-full p-6">
+				<Outlet />
+			</div>
+		</div>
+	);
 }
